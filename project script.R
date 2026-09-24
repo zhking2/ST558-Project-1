b517b8bd4bca7f853809edcdd1d54a055cdb2bba
lunalachrymose <lunalachrymose@gmail.com>
2026-09-24 14:07:18 -0400

    starting work on the project!

---
+++ project script.R
@@ -0,0 +1,9 @@
+library(httr)
+
+
+
+
+l21<-"https://api.census.gov/data/2021/acs/acs1/pums/variables.json"
+l22<-"https://api.census.gov/data/2022/acs/acs1/pums/variables.json"
+l23<-"https://api.census.gov/data/2023/acs/acs1/pums/variables.json"
+l24<-"https://api.census.gov/data/2024/acs/acs1/pums/variables.json"
